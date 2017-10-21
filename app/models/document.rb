19c8f76ef3e793ff601d60cd3ad69fe9814c26c5
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-10-21 14:23:53 +1100

    Annotate models

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: documents
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Document < ApplicationRecord
     has_many :photos
     accepts_nested_attributes_for :photos
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id          :integer          not null, primary key
+#  document_id :integer
+#  image_data  :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Photo < ApplicationRecord
   belongs_to :document
 end
